01808e6b7499091d7b8903b581f36f40659fd263
oscar <morenoscar96@gmail.com>
2019-06-06 08:33:44 -0500

    fixing project first try

---
+++ burger_master/src/components/Burger/Burger.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import classes from './Burger.css'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])].map((_, i) => (
+                <BurgerIngredient key={ingKey + i} type={ingKey} />
+            ));
+        })
+        .reduce((arr,el) => {
+            return arr.concat(el)
+        },[]);
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default burger;
